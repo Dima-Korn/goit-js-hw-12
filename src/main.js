cf4516311a768d62cc85fb97282989abecee0d87
Dima-Korn <kornienkod1989@gmail.com>
2024-04-17 14:33:15 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,27 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import { fetchImages } from "./js/pixabay-api";
-import { renderImages, showLoadingAnimation, hideLoadingAnimation, clearGallery } from "./js/render-functions";
+import { fetchImages } from './js/pixabay-api';
+import {
+  renderImages,
+  showLoadingAnimation,
+  hideLoadingAnimation,
+  clearGallery,
+} from './js/render-functions';
 
-const loadMoreBtn = document.querySelector(".load-more-btn");
-const searchForm = document.getElementById("search-form");
-const searchInput = document.getElementById("search-input");
-const searchBtn = document.querySelector(".search-btn");
-searchForm.addEventListener("submit", onFormSubmit);
-loadMoreBtn.addEventListener("click", onBtnClick);
+const loadMoreBtn = document.querySelector('.load-more-btn');
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.querySelector('.search-btn');
+
+searchForm.addEventListener('submit', onFormSubmit);
+loadMoreBtn.addEventListener('click', onBtnClick);
 
 const perPage = 15;
 let page = 1;
-let query = "";
+let query = '';
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
@@ -26,40 +32,46 @@ async function onFormSubmit(event) {
   event.preventDefault();
   searchBtn.disabled = true;
   showLoadingAnimation();
-  clearGallery();
-  loadMoreBtn.style.display = "none";
+  clearGallery(); //
+  loadMoreBtn.style.display = 'none';
   query = searchInput.value.trim();
-  if (query !== "") {
+  if (query !== '') {
     page = 1;
     try {
       const images = await fetchImages(query, page);
-      searchInput.value = "";
-      if (!images || !images.data || !images.data.hits || images.data.hits.length === 0 || images.totalHits === 0) {
+      searchInput.value = ''; // Очищуємо поле вводу
+      if (
+        !images ||
+        !images.data ||
+        !images.data.hits ||
+        images.data.hits.length === 0 ||
+        images.totalHits === 0
+      ) {
         iziToast.info({
-          title: "Information",
-          message: "Вибачте, за вашим запитом не знайдено зображень. Будь ласка, спробуйте ще раз!",
-          position: "topRight",
-          backgroundColor: "red",
-          maxWidth: "500px"
+          title: 'Information',
+          message:
+            'Вибачте, за вашим запитом не знайдено зображень. Будь ласка, спробуйте ще раз!',
+          position: 'topRight',
+          backgroundColor: 'red',
+          maxWidth: '500px',
         });
       } else {
         renderImages(images.data.hits);
         lightbox.refresh();
         if (images.data.hits.length < perPage) {
-          loadMoreBtn.style.display = "none";
+          loadMoreBtn.style.display = 'none';
         } else {
-          loadMoreBtn.style.display = "block";
+          loadMoreBtn.style.display = 'block';
         }
       }
     } catch (error) {
-      console.error("Помилка під час отримання зображень:", error);
+      console.error('Помилка під час отримання зображень:', error);
     } finally {
       searchBtn.disabled = false;
       hideLoadingAnimation();
     }
   }
 }
-
 
 async function onBtnClick() {
   page += 1;
@@ -69,41 +81,38 @@ async function onBtnClick() {
 
     renderImages(images.data.hits);
 
-    const totalHits = images.totalHits;
+    const totalHits = images.data.totalHits;
 
-    if ((page * perPage) >= totalHits || images.data.hits.length < perPage) {
-      loadMoreBtn.style.display = "none";
-    } else {
-      loadMoreBtn.style.display = "block";
+    if (page * perPage >= totalHits || images.data.hits.length < perPage) {
+      loadMoreBtn.style.display = 'none';
     }
 
-    if (images.data.hits.length < perPage) {
-      loadMoreBtn.style.display = "none"; 
-    }
-
-    const card = document.querySelector(".gallery-item");
-
+    const card = document.querySelector('.gallery-item');
     if (card) {
       const cardHeight = card.getBoundingClientRect().height;
       window.scrollBy({
         left: 0,
         top: cardHeight * 2,
-        behavior: "smooth"
+        behavior: 'smooth',
       });
     }
-    if (images.data.hits.length === 0 || (totalHits > 0 && page >= Math.ceil(totalHits / perPage))) {
-      loadMoreBtn.style.display = "none";
+
+    if (
+      images.data.hits.length === 0 ||
+      (images.data.hits.length < perPage && page > 1)
+    ) {
+      // Якщо отримано порожній масив зображень або отримано менше зображень, ніж потрібно для однієї сторінки і це не перша сторінка
+      loadMoreBtn.style.display = 'none';
       iziToast.info({
-        title: "Information",
-        message: "Вибачте, але ви досягли кінця результатів пошуку.",
-        position: "topRight",
-        backgroundColor: "red",
-        maxWidth: "500px"
+        title: 'Information',
+        message: 'Вибачте, але ви досягли кінця результатів пошуку.',
+        position: 'topRight',
+        backgroundColor: 'red',
+        maxWidth: '500px',
       });
-      loadMoreBtn.style.display = "none";
     }
   } catch (error) {
-    console.error("Error fetching images:", error);
+    console.error('Error fetching images:', error);
     alert(error.message);
   } finally {
     hideLoadingAnimation();
